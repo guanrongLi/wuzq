0c46b8813d3121dcb7cd79e7adbe48f810adf4bf
lgrr <lgr123@stu.xjtu.edu.cn>
2019-03-18 19:30:24 +0800

    五子棋AI展示

---
+++ wuzq/MainWindow.xaml.cs
@@ -0,0 +1,364 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Windows.Threading;
+using System.Net.Sockets;
+using System.Net;
+
+namespace wuzq
+{
+    /// <summary>
+    /// MainWindow.xaml 的交互逻辑
+    /// </summary>
+
+    public partial class MainWindow : Window
+    {
+        enum GameType { People, Computer };
+        enum GameTurn { Black, White };
+
+        Thread aigameThread;
+        bool isPlaying = false;
+
+        Dictionary<GameTurn, GameType> type = new Dictionary<GameTurn, GameType>();
+        Dictionary<GameTurn, AIInterface> ai = new Dictionary<GameTurn, AIInterface>();
+
+        GameTurn gameTurn = GameTurn.Black;
+        bool isEnd = false;
+
+        int lastX = 15, lastY = 15;
+        double step = 612.5 / 14.0;
+        double Top = 8.75, Left = 8.75;
+        double R = 18.75;
+
+        Brush halfBlack = new SolidColorBrush(Color.FromArgb(127, 0, 0, 0));
+        Brush halfWhite = new SolidColorBrush(Color.FromArgb(127, 255, 255, 255));
+        Brush black = new SolidColorBrush(Color.FromArgb(255, 0, 0, 0));
+        Brush white = new SolidColorBrush(Color.FromArgb(255, 255, 255, 255));
+
+        int[,] board = new int[16, 16];
+        int[,] _board = new int[16, 16];
+
+        Ellipse[,] whitePieces = new Ellipse[32, 32];
+        Ellipse[,] blackPieces = new Ellipse[32, 32];
+
+        //public NetworkInterface Network;
+        public MainWindow()
+        {
+            InitializeComponent();
+            type[GameTurn.Black] = GameType.People;
+            type[GameTurn.White] = GameType.People;
+            for (int i = 0; i < 15; i++)    //初始化棋盘
+            {
+                for (int j = 0; j < 15; j++)
+                {
+                    double x = Top + step * i;
+                    double y = Left + step * j;
+
+                    whitePieces[i, j] = MakeVirtualPieces(x, y, halfWhite);
+                    blackPieces[i, j] = MakeVirtualPieces(x, y, halfBlack);
+                }
+            }
+
+            whitePieces[15, 15] = blackPieces[15, 15] = new Ellipse();
+            people_people.IsChecked = true;
+            groupBox.Visibility = Visibility.Collapsed;
+            groupBox1.Visibility = Visibility.Collapsed;
+            groupBox2.Visibility = Visibility.Collapsed;
+        }
+
+        public Ellipse MakeVirtualPieces(double x, double y, Brush brush)  //制作虚拟棋子
+        {
+            Ellipse result = new Ellipse();
+            result.Fill = brush;
+            result.Width = R * 2;
+            result.Height = R * 2;
+            chessBoard.Children.Add(result);
+            Canvas.SetLeft(result, x);
+            Canvas.SetTop(result, y);
+            result.Visibility = Visibility.Hidden;
+            return result;
+        }
+        private void HideLastVirtualPiece()     //隐藏虚拟棋子
+        {
+            if (board[lastX, lastY] != 0)
+                return;
+            if (gameTurn == GameTurn.Black)
+                blackPieces[lastX, lastY].Visibility = Visibility.Hidden;
+            else
+                whitePieces[lastX, lastY].Visibility = Visibility.Hidden;
+        }
+        private void ShowVirtualPiece(int x, int y)    //显示虚拟棋子
+        {
+            if (board[x, y] != 0)
+                return;
+            if (gameTurn == GameTurn.Black)
+                blackPieces[x, y].Visibility = Visibility.Visible;
+            else
+                whitePieces[x, y].Visibility = Visibility.Visible;
+        }
+        private void PutPiece(int x, int y)    //落子及判断胜负函数
+        {
+            if (board[x, y] != 0)
+                return;
+            if (gameTurn == GameTurn.Black)   //落黑子
+            {
+                board[x, y] = 1;
+                HideLastVirtualPiece();
+                blackPieces[x, y].Visibility = Visibility.Visible;
+                blackPieces[x, y].Fill = black;
+                gameTurn = GameTurn.White;
+            }
+            else    //落白子
+            {
+                board[x, y] = 2;
+                whitePieces[lastX, lastY].Visibility = Visibility.Hidden;
+                whitePieces[x, y].Visibility = Visibility.Visible;
+                whitePieces[x, y].Fill = white;
+                gameTurn = GameTurn.Black;
+            }
+
+            lastX = lastY = 15;
+            int result = Util.CheckBoardResult(board, x, y);  //调用Until类中判断函数
+            if (result == 1)
+            {
+                MessageBox.Show("黑棋获胜，游戏结束");
+                isEnd = true;
+            }
+
+            if (result == 2)
+            {
+                MessageBox.Show("白棋获胜，游戏结束");
+                isEnd = true;
+            }
+            if (!isEnd && type[gameTurn] == GameType.Computer)   //若轮到电脑，使用AI落子
+                UseAI(ai[gameTurn] as AIInterface);
+
+        }
+        /// <summary>
+        /// 判断最终结果类
+        /// </summary>
+        public class Util
+        {
+            static int[] dx = { 0, 1, 1, 1 };
+            static int[] dy = { 1, -1, 0, 1 };
+            private static bool InRange(int x, int y)
+            {
+                return x >= 0 && x < 15 && y >= 0 && y < 15;
+            }
+
+            public static int CheckBoardResult(int[,] board, int x, int y)  
+                //黑棋胜返回1，白棋胜返回2，平局返回3，不分胜负返回0
+            {
+                int now = board[x, y];
+                for (int d = 0; d < 4; d++)
+                {
+                    int s = 1;
+                    for (int i = 1; true; i++)
+                    {
+                        int nowX = x + dx[d] * i, nowY = y + dy[d] * i;
+                        if (!InRange(nowX, nowY) || board[nowX, nowY] != now)
+                            break;
+                        s++;
+                    }
+
+                    for (int i = 1; true; i++)
+                    {
+                        int nowX = x - dx[d] * i, nowY = y - dy[d] * i;
+                        if (!InRange(nowX, nowY) || board[nowX, nowY] != now)
+                            break;
+                        s++;
+                    }
+
+                    if (s > 4)
+                        return now;
+                }
+                for(int i=0;i<board.Rank;i++)
+                    for(int j=0;j<board.GetLength(0);j++)
+                    {
+                        if(board[i,j]==0)
+                        {
+                            return 0;
+                        }
+                    }
+                return 3;
+            }
+        }
+        private void CopyBoard()   //复制棋盘，用于AI运算
+        {
+            for (int i = 0; i < 15; i++)
+                for (int j = 0; j < 15; j++)
+                    _board[i, j] = board[i, j];
+        }
+        private void UseAI(AIInterface ai)   //AI操作函数
+        {
+            int now = 1;
+            if (gameTurn != GameTurn.Black)
+                now = 2;
+            CopyBoard();
+            ai.Running(_board, now);
+            int x, y;
+            ai.GetNextStep(out x, out y);
+            PutPiece(x, y);
+        }
+        private void restart()  //重置棋盘函数
+        {
+            if (aigameThread != null)
+            {
+                aigameThread.Abort();
+                aigameThread.Join();
+                isPlaying = false;
+            }
+
+            isEnd = false;
+            for (int i = 0; i < 15; i++)
+                for (int j = 0; j < 15; j++)
+                {
+                    blackPieces[i, j].Fill = halfBlack;
+                    whitePieces[i, j].Fill = halfWhite;
+                    blackPieces[i, j].Visibility = Visibility.Hidden;
+                    whitePieces[i, j].Visibility = Visibility.Hidden;
+                    board[i, j] = 0;
+                    gameTurn = GameTurn.Black;
+                }
+        }
+        //后续代码均为事件处理
+        private void chessBoard_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            if (type[gameTurn] == GameType.Computer || isEnd || isPlaying)
+                return;
+            double xPos = e.GetPosition(chessBoard).X;
+            double yPos = e.GetPosition(chessBoard).Y;
+            int x = Convert.ToInt32((xPos) / step - 0.5);
+            int y = Convert.ToInt32((yPos) / step - 0.5);
+            if (x >= 0 && x < 15 && y >= 0 && y < 15)
+                PutPiece(x, y);
+        }
+
+        private void chessBoard_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (type[gameTurn] == GameType.Computer || isEnd || isPlaying)
+                return;
+            double xPos = e.GetPosition(chessBoard).X;
+            double yPos = e.GetPosition(chessBoard).Y;
+            int x = Convert.ToInt32((xPos) / step - 0.5);
+            int y = Convert.ToInt32((yPos) / step - 0.5);
+            if (x >= 0 && x < 15 && y >= 0 && y < 15)
+            {
+                HideLastVirtualPiece();
+                ShowVirtualPiece(x, y);
+                lastX = x;
+                lastY = y;
+            }
+        }
+
+        private void 人机对战_Click(object sender, RoutedEventArgs e)
+        {
+            restart();
+            groupBox.Visibility = Visibility.Collapsed;
+            groupBox1.Visibility = Visibility.Collapsed;
+            groupBox2.Visibility = Visibility.Collapsed;
+            MessageBoxResult result = MessageBox.Show(this, "AI是否先行?", "" ,MessageBoxButton.YesNo);
+            if (result == MessageBoxResult.Yes)
+            {
+                type[GameTurn.Black] = GameType.Computer;
+                ai[GameTurn.Black] = new ZtxzAI();
+                type[GameTurn.White] = GameType.People;
+            }
+            else
+            {
+                type[GameTurn.Black] = GameType.People;
+                ai[GameTurn.White] = new ZtxzAI();
+                type[GameTurn.White] = GameType.Computer;
+            }
+            if(type[GameTurn.Black]==GameType.Computer)
+                UseAI(ai[GameTurn.Black]);
+        }
+
+        private void people_people_Click(object sender, RoutedEventArgs e)
+        {
+            groupBox.Visibility = Visibility.Collapsed;
+            groupBox1.Visibility = Visibility.Collapsed;
+            groupBox2.Visibility = Visibility.Collapsed;
+            type[GameTurn.Black] = GameType.People;
+            type[GameTurn.White] = GameType.People;
+            restart();
+        }
+
+        private void start_Click(object sender, RoutedEventArgs e)
+        {
+            restart();
+            if (type[GameTurn.Black] == GameType.Computer)
+                UseAI(ai[GameTurn.Black]);
+        }
+
+        private void internet_Click(object sender, RoutedEventArgs e)
+        {
+            //restart();
+            //radioButton.IsChecked = true;
+            //groupBox.Visibility = Visibility.Visible;
+            //groupBox1.Visibility = Visibility.Visible;
+            //groupBox2.Visibility = Visibility.Collapsed;
+        }
+
+        private void radioButton_Click(object sender, RoutedEventArgs e)
+        {
+            groupBox1.Visibility = Visibility.Visible;
+            groupBox2.Visibility = Visibility.Collapsed;
+        }
+
+        private void radioButton1_Click(object sender, RoutedEventArgs e)
+        {
+            groupBox2.Visibility = Visibility.Visible;
+            groupBox.Visibility = Visibility.Collapsed;
+            groupBox1.Visibility = Visibility.Visible;
+        }
+
+        private void found_Click(object sender, RoutedEventArgs e)
+        {
+            //try
+            //{
+            //    int port = int.Parse(textBox1.Text);
+            //    this.Network.Start(port);
+            //}
+            //catch (Exception ex)
+            //{
+            //    MessageBox.Show(this, ex.Message, string.Empty, MessageBoxButton.OK);
+            //    return;
+            //}
+        }
+
+        private void connect_Click(object sender, RoutedEventArgs e)
+        {
+            //try
+            //{
+            //    IPAddress ip = IPAddress.Parse(textBox1.Text);
+            //    int port = int.Parse(textBox2.Text);
+
+            //    IPEndPoint end = new IPEndPoint(ip, port);
+            //    Network.Connect(end);
+            //}
+            //catch (Exception ex)
+            //{
+            //    MessageBox.Show(this, ex.Message, string.Empty, MessageBoxButton.OK);
+            //    return;
+            //}
+        }
+
+        private void quit_Click_1(object sender, RoutedEventArgs e)
+        {
+            App.Current.Shutdown();
+        }
+    }
+}
---
+++ wuzq/NetworkInterface.cs
@@ -0,0 +1,339 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Net;
+using System.Net.Sockets;
+using System.Threading;
+
+namespace wuzq
+{
+
+    public enum MessageType
+    {
+        None = 0,
+        Sucess,
+        Error,
+
+        Reset = 8,
+        Piece,
+        Set,
+        Pass,
+    }
+
+    public static class StreamHead
+    {
+        const int HeadLengthIndex = 0;
+        const int MessageTypeIndex = 4;
+        const int PointXIndex = 8;
+        const int PointYIndex = 12;
+        const int ChatIndex = 16;
+
+        public static void WriteHead(byte[] array, MessageType type, int x, int y)
+        {
+            int HeadLength = 16 ;
+            Array.Copy(BitConverter.GetBytes(HeadLength), 0, array, HeadLengthIndex, 4);
+            Array.Copy(BitConverter.GetBytes((int)type), 0, array, MessageTypeIndex, 4);
+            Array.Copy(BitConverter.GetBytes(x), 0, array, PointXIndex, 4);
+            Array.Copy(BitConverter.GetBytes(y), 0, array, PointYIndex, 4);
+        }
+
+        public static void Read(byte[] array, out MessageType type, out int x, out int y)
+        {
+            type = (MessageType)BitConverter.ToInt32(array, MessageTypeIndex);
+            x = BitConverter.ToInt32(array, PointXIndex);
+            y = BitConverter.ToInt32(array, PointYIndex);
+            int length = BitConverter.ToInt32(array, HeadLengthIndex);
+        }
+    }
+    public class NetworkInterface
+    {
+        public delegate void Act();
+
+        public MainWindow Owner { get; private set; }
+
+        public event EventHandler ClientConnected;
+        public event EventHandler Connected;
+        public event EventHandler AbortedByException;
+
+        public bool Running
+        {
+            get
+            {
+                return server != null || socket != null || acceptThread != null || listenThread != null || sendThread != null;
+            }
+        }
+
+        List<byte[]> messages;
+
+        Random random;
+        Socket server;
+        Socket socket;
+        Thread acceptThread;
+        Thread listenThread;
+        Thread sendThread;
+
+        public NetworkInterface(object obj)    //网络接口
+        {
+            if (obj == null || obj.GetType() != typeof(MainWindow))
+            {
+                throw new Exception("无效的父对象!");
+            }
+
+            Owner = (MainWindow)obj;
+            messages = new List<byte[]>();
+        }
+
+        public void Start(int port)    //建立服务器
+        {
+            if (Running)
+            {
+                throw new Exception("对象非空!");
+            }
+            try
+            {
+                server = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);   //建立接口
+                server.Bind(new IPEndPoint(IPAddress.Any, port));
+                server.Listen(1);   //最大允许一个连入
+            }
+            catch
+            {
+                if (server != null)
+                {
+                    server.Close();
+                    server = null;
+                }
+                throw;
+            }
+
+            acceptThread = new Thread(accept);
+            acceptThread.Start();   //开始监听
+        }
+
+        void accept()    //收到信息
+        {
+            socket = server.Accept();    //为连接创建新的socket
+            server.Close();
+            server = null;    //关闭服务器
+
+            messages.Clear();
+
+            listenThread = new Thread(receiver);    //创建接受流和发送流
+            listenThread.Start();
+
+            sendThread = new Thread(sender);
+            sendThread.Start();
+
+            if (ClientConnected != null)
+            {
+                ClientConnected(this, new EventArgs());
+            }
+        }
+
+        public void Connect(IPEndPoint ip)    //客户端连接
+        {
+            if (Running)
+            {
+                throw new Exception("对象非空!");
+            }
+
+            try
+            {
+                socket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+                socket.Connect(ip);
+            }
+            catch
+            {
+                if (socket != null)
+                {
+                    socket.Close();
+                    socket = null;
+                }
+                throw;
+            }
+
+            messages.Clear();
+
+            listenThread = new Thread(receiver);
+            listenThread.Start();
+
+            sendThread = new Thread(sender);
+            sendThread.Start();
+
+            if (Connected != null)
+            {
+                Connected(this, new EventArgs());
+            }
+        }
+
+        public void Send(MessageType type, int x = 0, int y = 0)   //发送信息
+        {
+            int length = 16 ;
+            byte[] head = new byte[length];
+            StreamHead.WriteHead(head, type, x, y);
+
+            lock (messages)   //线程锁
+            {
+                messages.Add(head);
+            }
+        }
+
+        void receiver()    //接收端
+        {
+            while (true)
+            {
+                MessageType type;
+                int x, y;
+
+                byte[] getLength = new byte[4];
+                int alllength;
+                try
+                {
+                    alllength = socket.Receive(getLength, 4, SocketFlags.None);  //接受数据
+                }
+                catch(Exception e)
+                {
+                    Console.WriteLine(e.Message);
+                    goto End;
+                }
+                alllength = BitConverter.ToInt32(getLength, 0);
+                byte[] head = new byte[alllength];
+                byte[] temp = new byte[alllength];
+                Array.Copy(getLength, head, 4);
+
+                try
+                {
+                    int sub = alllength - 4;
+                    int length;
+                    while (true)
+                    {
+                        int l = (temp.Length < sub) ? temp.Length : sub;
+                        length = socket.Receive(temp, l, SocketFlags.None);
+                        if (length <= 0)
+                        {
+                            goto End;
+                        }
+                        if (length == sub)
+                        {
+                            Array.Copy(temp, 0, head, head.Length - sub, length);
+                            break;
+                        }
+                        else // if (length < sub)
+                        {
+                            Array.Copy(temp, 0, head, head.Length - sub, length);
+                            sub -= length;
+                        }
+                    }
+                    StreamHead.Read(head, out type, out x, out y);
+
+                    if (type == MessageType.Reset)
+                    {
+                        MainWindow.Piece self = random.Next(0, 2) == 0 ? MainWindow.Piece.Black : MainWindow.Piece.White;
+                        MainWindow.Piece now = MainWindow.Piece.Black;
+
+                        // 先设置棋子 再请求刷新 否则可能不一致
+                        Owner.RefreshText(self, now);
+                        Owner.Reset();
+
+                        Send(MessageType.Piece, (int)MainWindow.Reserve(self), (int)now);
+                    }
+                    if (type == MessageType.Piece)
+                    {
+                        
+
+                        Owner.RefreshText(self, now);
+                        Owner.Reset();
+                    }
+                    if (type == MessageType.Set)
+                    {
+                        Owner.Set(new Point(x, y), false);
+                    }
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine(e.Message);
+                    goto End;
+                }
+            }
+
+        End:
+            new Thread(abort).Start();
+        }
+
+        void sender()     //发送端
+        {
+            while (true)
+            {
+                byte[] temp = null;
+
+                lock (messages)
+                {
+                    if (messages.Count > 0)
+                    {
+                        temp = messages[0];
+                        messages.RemoveAt(0);
+                    }
+                }
+
+                if (temp != null)
+                {
+                    try
+                    {
+                        socket.Send(temp);
+                    }
+                    catch (Exception e)
+                    {
+                        Console.WriteLine(e.Message);
+                        goto End;
+                    }
+                }
+                else
+                {
+                    Thread.Sleep(1);
+                }
+            }
+
+        End:
+            new Thread(abort).Start();
+        }
+
+        public void Abort()         //终止进程
+        {
+            if (server != null)
+            {
+                server.Close();
+                server = null;
+            }
+            if (socket != null)
+            {
+                socket.Close();
+                socket = null;
+            }
+            if (acceptThread != null)
+            {
+                acceptThread.Abort();
+                acceptThread = null;
+            }
+            if (listenThread != null)
+            {
+                listenThread.Abort();
+                listenThread = null;
+            }
+            if (sendThread != null)
+            {
+                sendThread.Abort();
+                sendThread = null;
+            }
+        }
+
+        void abort()
+        {
+            Abort();
+
+            if (AbortedByException != null)
+            {
+                AbortedByException(this, new EventArgs());
+            }
+        }
+    }
+
+}
